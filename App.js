e178f0e963f0cb82e4a469dbf416cbf8e156fd2c
raka <rakasandheo160304@gmail.com>
2024-11-24 22:46:05 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.centerText}>Salam kenal saya raka sandhheo rambani dengan NPM 223510641</Text>
+
+      <View style={styles.boxContainer}>
+        <View style={[styles.box, styles.box1]} />
+        <View style={[styles.box, styles.box2]} />
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#d3d3d3', 
+  },
+  centerText: {
+    fontSize: 15,
+    color: 'black',
+    fontWeight: 'bold',
+    marginBottom: 50, 
+  },
+  boxContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%', 
+  },
+  box: {
+    width: 100,
+    height: 100,
+  },
+  box1: {
+    backgroundColor: 'brown', 
+  },
+  box2: {
+    backgroundColor: 'grey', 
   },
 });
+
+export default App;
